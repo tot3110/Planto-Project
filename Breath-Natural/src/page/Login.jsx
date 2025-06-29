9a3958816fb10bc279250acc5d7536033766da0b
tot3110 <abasit3110@gmail.com>
2025-06-29 15:57:02 +0500

    Build Login Page

--- Breath-Natural/src/page/Login.jsx
+++ Breath-Natural/src/page/Login.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
+import Loginimage from '../assets/Image 2.png';
 import { FaLock } from 'react-icons/fa';
 import { FaEnvelope } from 'react-icons/fa';
-
 
 const Login = () => {
   return (
     <div className="w-full h-screen bg-gradient-to-r from-[#AC72A1] via-[#FBD9FA] to-[#070E2A] 
     flex items-center justify-center">
-  {/* Wrapper for both sides */}
-  <div className="flex w-full h-full rounded-[100px] overflow-hidden">
-    
+    {/* Wrapper for both sides */}
+    <div className="flex w-full h-full">
+          
     {/* Left side - IMAGE */}
-    <div
-      className="w-full bg-cover bg-center">
-        <img src= {`$`} alt="" />
-      </div>
+    <div className="flex justify-end items-center bg-center w-[50%]">
+        <div className='w-[550px] h-[500px] relative rounded-[40px] bg-[#000000]'>
+          <img src= {`${Loginimage}`} className='object-cover h-[600px] absolute top-[-80px]' />
+        </div>
+    </div>
 
     {/* Right side - LOGIN FORM */}
-    <div className="w-[800px] h-[525px] bg-white/10 backdrop-blur-md p-10 text-white flex flex-col justify-center">
-      <h2 className="text-3xl font-bold text-center mb-8 drop-shadow">Login</h2>
+    <div className='flex justify-start items-center bg-center w-[50%]'>
+      <div className="w-[550px] h-[400px] rounded-[40px] p-5 bg-white/10 backdrop-blur-md text-white flex flex-col items-center justify-center">
+      <h2 className="text-3xl font-bold text-center text-[#AC72A1] mb-8">Login</h2>
 
       {/* Email */}
-      <div className="relative mb-6">
+      <div className="w-[60%] relative mb-6">
         <FaEnvelope className="absolute top-1/2 right-3 transform -translate-y-1/2 text-black text-lg" />
         <input
           type="email"
@@ -32,7 +34,7 @@ const Login = () => {
 
 
       {/* Password */}
-      <div className="relative mb-6">
+      <div className="w-[60%] relative mb-6">
         <FaLock className="absolute top-1/2 right-3 transform -translate-y-1/2 text-black text-lg" />
         <input
           type="password"
@@ -42,20 +44,21 @@ const Login = () => {
       </div>
 
       {/* Button */}
-      <button className="w-full bg-gradient-to-r from-purple-500 to-pink-500 py-3 rounded-full hover:opacity-90 transition">
+      <button className="w-[60%] bg-gradient-to-r from-[#AC72A1] to-[#070E2A] 
+      py-3 rounded-full hover:opacity-90 transition">
         Login
       </button>
 
       {/* Links */}
-      <div className="flex justify-between mt-6 text-sm text-white/80">
-        <a href="#" className="hover:underline">Create an account</a>
-        <a href="#" className="hover:underline">Forgot password?</a>
+      <div className="w-[60%] flex justify-between mt-6 text-sm text-white/80">
+        <a href="#" className="hover:underline text-[#070E2A]">Create an account</a>
+        <a href="#" className="hover:underline text-[#070E2A]">Forgot password?</a>
       </div>
     </div>
-  </div>
-</div>
-  );
+    </div>
+    </div>
+    </div>
+);
 };
 
 export default Login;
-
